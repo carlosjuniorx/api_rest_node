440a2878316abb39b9a91ca6cf0fe6ae631b7fee
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2021-12-30 20:37:01 -0300

    Feito o CRUD de Usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,73 @@ class UserController {
   async store(req,res){
     try {
       const novoUser = await User.create(req.body)
-      res.json(novoUser)
+      return res.json(novoUser)
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: error.errors.map((err)=> err.message)
+      })
+    }
+  }
+
+  async index(req, res){
+    try {
+      const users = await User.findAll()
+      return res.json(users)
+    }catch(error){
+      return res.json(null)
+    }
+  }
+
+  async show(req, res){
+    try {
+      const user = await User.findByPk(req.params.id)
+      return res.json(user)
+    } catch (error) {
+      return res.json(null)
+    }
+  }
+
+  async update(req, res){
+    try {
+      if(!req.params.id){
+        return res.status(400).json({
+          errors:['ID não enviado.']
+        })
+      }
+      const user = await User.findByPk(req.params.id)
+      if(!user){
+        return res.status(400).json({
+          erros: ['Usuário não exister']
+        })
+      }
+      const atualizado = await user.update(req.body)
+      return res.json(atualizado)
+
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err)=> err.message)
+      })
+    }
+  }
+
+  async delete(req, res){
+    try {
+      if(!req.params.id){
+        return res.status(400).json({
+          errors:['ID não enviado.']
+        })
+      }
+      const user = await User.findByPk(req.params.id)
+      if(!user){
+        return res.status(400).json({
+          erros: ['Usuário não exister']
+        })
+      }
+      await user.destroy()
+      return res.json(user)
+
+    } catch (error) {
+      return res.status(400).json({
         errors: error.errors.map((err)=> err.message)
       })
     }
