1ad2448e6cbca0d7353b65042ef97e215e9e7f5f
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2021-12-29 21:01:54 -0300

    Criado as depêndencias de usuários

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import './src/database'
 
 import express from 'express'
 import homeRoutes from './src/routes/homeRoutes'
+import userRoutes from './src/routes/userRoutes'
 
 class App {
   constructor(){
@@ -11,15 +12,15 @@ class App {
     this.middlewares()
     this.routes()
   }
+
   middlewares(){
     this.app.use(express.urlencoded({extended: true}))
     this.app.use(express.json())
-
   }
 
   routes(){
     this.app.use('/', homeRoutes)
-
+    this.app.use('/users/', userRoutes)
   }
 }
 
