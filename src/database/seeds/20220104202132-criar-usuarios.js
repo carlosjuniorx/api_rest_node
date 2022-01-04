9119acab1b1504de3b932bf77bdcdce39411b574
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2022-01-04 17:36:50 -0300

    teste de seed

---
+++ src/database/seeds/20220104202132-criar-usuarios.js
@@ -0,0 +1,20 @@
+const bcryptjs = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface) => {
+
+    await queryInterface.bulkInsert('users', [{
+      nome: 'John Doe',
+      email: "email@email.com",
+      password_hash: await bcryptjs.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    }],
+    {}
+    );
+  },
+
+  down: async () => {
+
+  }
+};
