c79c79e9daa005e84ec9e3c14be6024a02c2286d
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2022-01-04 18:52:35 -0300

    Feito rotas para Alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,7 @@
-import Aluno from '../models/Aluno'
 
 class HomeController {
   async index(req,res){
-    const novoAluno = await Aluno.create({
-      nome: 'Marciano',
-      sobrenome: 'Martes',
-      email: 'marciano@gmail.com',
-      idade: 112,
-      peso: 200,
-      altura: 1.80,
-    })
-    res.json(novoAluno)
+    res.json('index')
   }
 }
 
