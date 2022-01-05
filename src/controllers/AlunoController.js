3548410a956c3f4f0435be6580b1b4b3e30c8f24
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2022-01-05 17:24:48 -0300

    Crud de alunos concluido

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,100 @@ class AlunoController {
     const alunos = await Aluno.findAll()
     res.json(alunos)
   }
+
+  async store(req,res){
+    try {
+      const aluno = await Aluno.create(req.body)
+
+      return res.json(aluno)
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err)=> err.message)
+      })
+    }
+  }
+
+  async update(req,res){
+    try {
+      const {id} = req.params
+
+      if(!id){
+        return res.status(400).json({
+          errors: ['Faltando id']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+
+      if(!aluno){
+        return res.status(400).json({
+          errors: ['Aluno inexistente']
+        })
+      }
+
+      const alunoAtualizado = await aluno.update(req.body)
+
+      return res.json(alunoAtualizado)
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map(err => err.message)
+      })
+    }
+  }
+
+  async show(req,res){
+    try {
+      const {id} = req.params
+
+      if(!id){
+        return res.status(400).json({
+          errors: ['Faltando id']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+
+      if(!aluno){
+        return res.status(400).json({
+          errors: ['Aluno inexistente']
+        })
+      }
+      return res.json(aluno)
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map(err => err.message)
+      })
+    }
+  }
+
+  async delete(req,res){
+    try {
+      const {id} = req.params
+
+      if(!id){
+        return res.status(400).json({
+          errors: ['Faltando id']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id)
+
+      if(!aluno){
+        return res.status(400).json({
+          errors: ['Aluno inexistente']
+        })
+      }
+
+      await aluno.destroy()
+        return res.json({
+          apagado: true
+        })
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map(err => err.message)
+      })
+    }
+  }
 }
 
 export default new AlunoController()
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,12 +3,65 @@ import Sequelize, {Model} from 'sequelize'
 export default class Aluno extends Model{
   static init(sequelize){
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaulltValue: '',
+        validate: {
+          len:{
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          }
+        }
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaulltValue: '',
+        validate: {
+          len:{
+            args: [3, 255],
+            msg: 'Campo sobrenome deve ter entre 3 e 255 caracteres',
+          }
+        }
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaulltValue: '',
+        unique:{
+          msg: 'Email já existe'
+        },
+        validate: {
+          isEmail:{
+            msg: 'Email inválido',
+          }
+        }
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaulltValue: '',
+        validate: {
+          isInt:{
+            msg: 'Idade precisa ser um número inteiro',
+          }
+        }
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaulltValue: '',
+        validate: {
+          isFloat:{
+            msg: 'Peso precisa ser um número',
+          }
+        }
+      },
+      altura: {
+        type: Sequelize.STRING,
+        defaulltValue: '',
+        validate: {
+          isFloat:{
+            msg: 'Altura precisa ser um número',
+          }
+        }
+      },
     },{
       sequelize,
     })
