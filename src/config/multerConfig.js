db23457609e30c48e6224034d01c55e30940e5cf
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2022-01-06 18:15:23 -0300

    tratamento dos arquivos enviados

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,9 +1,15 @@
 import multer from 'multer'
-import {extname, resolve} from 'path'
+import {extname} from 'path'
 
 const aleatorio = () => Math.floor(Math.random()* 10000 + 100000)
 
 export default {
+  fileFilter: (req, file, cb)=>{
+    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg'){
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG.'))
+    }
+    return cb(null, true)
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb)=>{
       cb(null, 'uploads/')
--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,7 +1,18 @@
+import multer from 'multer'
+import multerConfig from '../config/multerConfig'
+
+const upload = multer(multerConfig).single('foto')
 
 class FotoController {
   async store(req,res){
-    res.json('ok')
+    return upload(req, res, (error)=>{
+      if(error){
+        return res.status(400).json({
+          errors: [error.code],
+        })
+      }
+      res.json(req.file)
+    })
   }
 }
 
