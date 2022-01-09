89d36976dc4195e643dd249eeaf5430347252eec
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2022-01-09 13:48:34 -0300

    tratamento dos arquivos staticos

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -12,7 +12,7 @@ export default {
   },
   storage: multer.diskStorage({
     destination: (req, file, cb)=>{
-      cb(null, 'uploads/')
+      cb(null, 'uploads/images')
     },
     filename: (req, file, cb)=>{
       cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`)
